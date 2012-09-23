1b18d226a56d6f0c9a979206e9de7fd736390ce3
ehuelsmann <ehuelsmann@f2e92c9b-f110-0410-8fa8-b724fd9af65c>
2012-09-23 10:52:35 +0000

    Promote trunk to 0.9.1-dev, following the tag of 0.9.0.
    git-svn-id: https://svn.common-lisp.net/cl-irc/trunk@223 f2e92c9b-f110-0410-8fa8-b724fd9af65c

--- variable.lisp
+++ variable.lisp
@@ -10,7 +10,7 @@
 
 (defconstant +soh+ #.(code-char 1))
 
-(defparameter *version* "0.8.0-dev")
+(defparameter *version* "0.9.1-dev")
 (defparameter *ctcp-version*
   (format nil "CL IRC library, cl-irc:~A:~A ~A"
           *version* (machine-type) (machine-version)))
